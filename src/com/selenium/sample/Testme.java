e5fa5e53d06e508199cf18dc6cc04d593f36b21f
Prashanth Sams <sams.prashanth@gmail.com>
2014-06-23 01:56:37 +0530

    updates from Sunday

--- src/com/selenium/sample/Testme.java
+++ src/com/selenium/sample/Testme.java
@@ -6,16 +6,16 @@ public class Testme {
 
 	public static void main(String Args[]) {
 		int i = 0;
-		report_may03 o = new report_may03();
-		String alph[] = { "3", "2", "6", "6", "6", "6", "6" };
-		for (i = 0; i <= 4; i++) {
+		report_imran o = new report_imran();
+		String alph[] = { "7", "5", "5", "5", "7", "5", "5","5", "7", "5", "5","5", "7", "5", "5", "6", "4"};
+		for (i = 0; i <= 14; i++) {
 			o.Addinput(alph[i], i);
 			o.Expectedoutput(alph[i+2], i);
 			o.Output(alph[i], i);
 			
 		}
 		try {
-			o.Generatereport(4);
+			o.Generatereport(14);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
